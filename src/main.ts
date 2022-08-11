276436fd11e35a16b1fd05319b5f975898667f97
borislavstoychev <stoy4ew@gmail.com>
2022-08-12 01:51:56 +0300

    heroku settings port

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  const port = process.env.API_PORT || 3310;
+  const port = process.env.PORT || 3310;
   await app.listen(port, () => {
     Logger.debug(
       `Listening at http://localhost:${port}`,
